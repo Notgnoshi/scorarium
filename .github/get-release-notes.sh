25e690de858fe9e2850917863e90004d62875498
Austin Gill <Notgnoshi@gmail.com>
2026-09-03 18:26:30 -0500

    Add the Docker image and musl binary to generated releases

--- .github/get-release-notes.sh
+++ .github/get-release-notes.sh
@@ -21,3 +21,13 @@ echo "$DESCRIPTION"
 # 0,/pat1/d deletes from line 1 to pat1 inclusive
 # /pat2/Q exits without printing on the first line to match pat2
 sed "0,/^# Scorarium - $VERSION -/d;/^# Scorarium - /Q" "$CHANGELOG"
+
+# The image and binary are published by the image job after this release is created
+cat <<EOF
+
+## Docker image
+
+    docker pull ghcr.io/notgnoshi/scorarium:v$VERSION
+
+https://github.com/Notgnoshi/scorarium/pkgs/container/scorarium
+EOF
